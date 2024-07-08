2a7f708efab4e6956c011aab79f2ce6287a36327
washington <lepexgamer102000@gmail.com>
2024-07-08 13:01:28 -0300

    fist commit

---
+++ AL 205 Array Basico/script.js
@@ -0,0 +1,13 @@
+var variavel1 = 'Maçã';
+var variavel2 = 'Uva';
+
+console.log(variavel1);
+console.log(variavel2);
+
+var lista_frutas = Array();
+lista_frutas[0] = 'Melão'
+lista_frutas[1] = 'Morango'
+lista_frutas[2] = 'caju'
+
+console.log(lista_frutas);
+document.write(lista_frutas);
